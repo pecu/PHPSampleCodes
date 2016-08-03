8ed5f2af2def14b6dd8efb6c4d857f1d5fb16294
pecu <pecu610@gmail.com>
2016-08-03 15:17:45 +0800

    SELECT OPTION AND SQL

--- budget/MyTrip.php
+++ budget/MyTrip.php
@@ -2,18 +2,42 @@
 include('link.php');
 include("header.php");
 
-$reportSQL1 = "SELECT `userdata`.`name`, `product`.`company`, `product`.`date`, `product`.`time`, `orderno`.`orderNum` FROM `orderno` LEFT JOIN `product` ON `orderno`.`productid` = `product`.`no` LEFT JOIN `userdata` ON `orderno`.`userid` = `userdata`.`id`";
+$reportSQL1 = "SELECT `userdata`.`name` FROM `orderno` LEFT JOIN `product` ON `orderno`.`productid` = `product`.`no` LEFT JOIN `userdata` ON `orderno`.`userid` = `userdata`.`id` GROUP BY `userdata`.`name`";
 
 //echo $reportSQL1."<br>";
 
 $result = mysql_query($reportSQL1);
 
+?>
+<center><table border=1><tr>
+<form action="MyTrip.php" method="post">
+	<select name="username">
+<?php
+	while( $row = mysql_fetch_array($result) )
+	{
+		echo "<option value=\"". $row[0] ."\">" . $row[0] . "</option>";
+	}
+?>
+	</select>
+	<button type="submit">Search</button>
+</form>
+</tr>
+
+<?php
+
+$username = $_POST["username"];
+//echo $username;
+
 //echo $result;
 
+$reportSQL1 = "SELECT `userdata`.`name`, `product`.`company`, `product`.`date`, `product`.`time`, `orderno`.`orderNum` FROM `orderno` LEFT JOIN `product` ON `orderno`.`productid` = `product`.`no` LEFT JOIN `userdata` ON `orderno`.`userid` = `userdata`.`id` WHERE `userdata`.`name` = '" .$username. "'";
+
+//echo $reportSQL1."<br>";
+$result = mysql_query($reportSQL1);
+//echo $result;
 echo "<center><table border=1><tr>";
 echo "<th>UserName</th><th>Company</th><th>Date</th><th>Time</th><th>Tickets</th>";
 echo "</tr>";
-
 while( $row = mysql_fetch_array($result) )
 {
 	//print_r($row);
@@ -25,7 +49,6 @@ while( $row = mysql_fetch_array($result) )
 	echo "</tr>";
 }
 echo "</table></center>";
-
 ?>
 
 <br>
